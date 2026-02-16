bd177c48ed7b39566d05ac5e455be97bfe2ba203
Poornimj <poornimj@metropolia.fi>
2026-02-16 15:36:24 +0200

    Update database connection password for security enhancement

--- ClassIQ_codes/src/main/java/Backend/db/DBConnection.java
+++ ClassIQ_codes/src/main/java/Backend/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mariadb://localhost:3306/classiq";
     private static final String USER = "root";
-    private static final String PASSWORD = "Dilmi2000@";
+    private static final String PASSWORD = "root123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
